7d85a62ff0fb768a960c356137892f50e13fc72d
Dale Ghent <daleg@elemental.org>
2022-03-12 16:50:04 -0500

    Add some languages

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyMetadata("Homepage", "https://daleghent.com/moon-angle")]
 
 //[Optional] Common tags that quickly describe your plugin
-[assembly: AssemblyMetadata("Tags", "moon,luna,lune,maan,mond,月亮,月,φεγγάρι,Луна")]
+[assembly: AssemblyMetadata("Tags", "moon,luna,lune,maan,mond,місяць,księżyc,tungl,ukwezi,月亮,月,φεγγάρι,Луна")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/daleghent/nina-moon-angle/blob/main/CHANGELOG.md")]
