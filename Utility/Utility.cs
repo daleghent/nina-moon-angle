f3a9a6642714eb7a628f2a4a74dbcb9ce885ff49
Dale Ghent <daleg@elemental.org>
2022-12-24 18:51:01 -0500

    Use NINA-provided sun/moon postion funcs instead of local ones

--- Utility/Utility.cs
+++ Utility/Utility.cs
@@ -28,7 +28,9 @@ namespace DaleGhent.NINA.MoonAngle {
         public static double CalculateMoonSeparation(Coordinates coordinates, ObserverInfo observerInfo) {
             var date = DateTime.UtcNow;
             var jd = AstroUtil.GetJulianDate(date);
-            var moonPosition = GetMoonPosition(date, jd, observerInfo);
+            var moonPosition = AstroUtil.GetMoonPosition(date, jd, observerInfo);
+
+            Logger.Trace($"Moon RA: {AstroUtil.HoursToHMS(moonPosition.RA)}, Dec: {AstroUtil.DegreesToDMS(moonPosition.Dec)}");
 
             var moonRaRadians = AstroUtil.ToRadians(AstroUtil.HoursToDegrees(moonPosition.RA));
             var moonDecRadians = AstroUtil.ToRadians(moonPosition.Dec);
@@ -45,43 +47,12 @@ namespace DaleGhent.NINA.MoonAngle {
             return thetaDegrees;
         }
 
-        public static NOVAS.SkyPosition GetMoonPosition(DateTime date, double jd, ObserverInfo observerInfo) {
-            var deltaT = AstroUtil.DeltaT(date);
-
-            var onSurface = new NOVAS.OnSurface() {
-                Latitude = observerInfo.Latitude,
-                Longitude = observerInfo.Longitude,
-                Height = observerInfo.Elevation,
-                Pressure = observerInfo.Pressure,
-                Temperature = observerInfo.Temperature,
-            };
-
-            var obs = new NOVAS.Observer() {
-                OnSurf = onSurface,
-                Where = (short)NOVAS.ObserverLocation.EarthSurface
-            };
-
-            var moon = new NOVAS.CelestialObject() {
-                Name = "Moon",
-                Number = (short)NOVAS.Body.Moon,
-                Star = new NOVAS.CatalogueEntry(),
-                Type = (short)NOVAS.ObjectType.MajorPlanetSunOrMoon
-            };
-
-            var moonPosition = new NOVAS.SkyPosition();
-
-            var jdTt = jd + AstroUtil.SecondsToDays(deltaT);
-            _ = NOVAS.Place(jdTt, moon, obs, deltaT, NOVAS.CoordinateSystem.EquinoxOfDate, NOVAS.Accuracy.Full, ref moonPosition);
-
-            Logger.Trace($"Moon RA: {AstroUtil.HoursToHMS(moonPosition.RA)}, Dec: {AstroUtil.DegreesToDMS(moonPosition.Dec)}");
-
-            return moonPosition;
-        }
-
         public static double CalculateSunSeparation(Coordinates coordinates, ObserverInfo observerInfo) {
             var date = DateTime.UtcNow;
             var jd = AstroUtil.GetJulianDate(date);
-            var sunPosition = GetSunPosition(date, jd, observerInfo);
+            var sunPosition = AstroUtil.GetSunPosition(date, jd, observerInfo);
+
+            Logger.Trace($"Sun RA: {AstroUtil.HoursToHMS(sunPosition.RA)}, Dec: {AstroUtil.DegreesToDMS(sunPosition.Dec)}");
 
             var sunRaRadians = AstroUtil.ToRadians(AstroUtil.HoursToDegrees(sunPosition.RA));
             var sunDecRadians = AstroUtil.ToRadians(sunPosition.Dec);
@@ -96,39 +67,6 @@ namespace DaleGhent.NINA.MoonAngle {
             Logger.Debug($"Sun angle: {thetaDegrees:0.00}");
 
             return thetaDegrees;
-        }
-
-        public static NOVAS.SkyPosition GetSunPosition(DateTime date, double jd, ObserverInfo observerInfo) {
-            var deltaT = AstroUtil.DeltaT(date);
-
-            var onSurface = new NOVAS.OnSurface() {
-                Latitude = observerInfo.Latitude,
-                Longitude = observerInfo.Longitude,
-                Height = observerInfo.Elevation,
-                Pressure = observerInfo.Pressure,
-                Temperature = observerInfo.Temperature,
-            };
-
-            var obs = new NOVAS.Observer() {
-                OnSurf = onSurface,
-                Where = (short)NOVAS.ObserverLocation.EarthSurface
-            };
-
-            var moon = new NOVAS.CelestialObject() {
-                Name = "Sun",
-                Number = (short)NOVAS.Body.Sun,
-                Star = new NOVAS.CatalogueEntry(),
-                Type = (short)NOVAS.ObjectType.MajorPlanetSunOrMoon
-            };
-
-            var sunPosition = new NOVAS.SkyPosition();
-
-            var jdTt = jd + AstroUtil.SecondsToDays(deltaT);
-            _ = NOVAS.Place(jdTt, moon, obs, deltaT, NOVAS.CoordinateSystem.EquinoxOfDate, NOVAS.Accuracy.Full, ref sunPosition);
-
-            Logger.Trace($"Sun RA: {AstroUtil.HoursToHMS(sunPosition.RA)}, Dec: {AstroUtil.DegreesToDMS(sunPosition.Dec)}");
-
-            return sunPosition;
         }
 
         public static IDeepSkyObject FindDsoInfo(ISequenceContainer container) {
