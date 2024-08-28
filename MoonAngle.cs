002529105554c8ec95906f2959ed91fa8a7bc87c
Dale Ghent <daleg@elemental.org>
2024-08-27 23:01:00 -0400

    Fold all logic into FinalizedImage event

--- MoonAngle.cs
+++ MoonAngle.cs
@@ -13,38 +13,28 @@
 using NINA.Astrometry;
 using NINA.Core.Model;
 using NINA.Core.Utility;
-using NINA.Core.Utility.Notification;
 using NINA.Image.ImageData;
 using NINA.Plugin;
 using NINA.Plugin.Interfaces;
-using NINA.Profile;
 using NINA.Profile.Interfaces;
 using NINA.WPF.Base.Interfaces.Mediator;
 using NINA.WPF.Base.Interfaces.ViewModel;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.IO;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 using Settings = DaleGhent.NINA.MoonAngle.Properties.Settings;
 
 namespace DaleGhent.NINA.MoonAngle {
+
     [Export(typeof(IPluginManifest))]
     public class MoonAngle : PluginBase, INotifyPropertyChanged {
-        private readonly IPluginOptionsAccessor pluginSettings;
         private readonly IProfileService profileService;
         private readonly IImageSaveMediator imageSaveMediator;
 
-        private const string sunAngleKeywordName = "SUNANGLE";
-        private const string sunAngleKeywordComment = "[deg] Angular separation between {0} and sun";
-        private readonly ImagePattern imagePatternSun = new ImagePattern("$$SUNANGLE$$", "Target's angular separation from the sun", "Moon Angle") { Value = "146.83" };
-
-        private const string moonAngleKeywordName = "MOONANGL";
-        private const string moonAngleKeywordComment = "[deg] Angular separation between {0} and moon";
-        private readonly ImagePattern imagePatternMoon = new ImagePattern("$$MOONANGLE$$", "Target's angular separation from the moon", "Moon Angle") { Value = "34.19" };
+        private readonly ImagePattern imagePatternSun = new("$$SUNANGLE$$", "Target's angular separation from the sun", "Moon Angle") { Value = "146.83" };
+        private readonly ImagePattern imagePatternMoon = new("$$MOONANGLE$$", "Target's angular separation from the moon", "Moon Angle") { Value = "34.19" };
 
         [ImportingConstructor]
         public MoonAngle(IProfileService profileService, IOptionsVM options, IImageSaveMediator imageSaveMediator) {
@@ -59,10 +49,7 @@ namespace DaleGhent.NINA.MoonAngle {
             options.AddImagePattern(imagePatternSun);
             options.AddImagePattern(imagePatternMoon);
 
-            this.imageSaveMediator.BeforeImageSaved += ImageSaveMediator_BeforeImageSaved;
             this.imageSaveMediator.BeforeFinalizeImageSaved += ImageSaveMediator_BeforeFinalizeImageSaved;
-
-            this.pluginSettings = new PluginOptionsAccessor(profileService, Guid.Parse(this.Identifier));
             this.profileService = profileService;
 
             profileService.ProfileChanged += ProfileService_ProfileChanged;
@@ -70,7 +57,6 @@ namespace DaleGhent.NINA.MoonAngle {
 
         public override Task Teardown() {
             profileService.ProfileChanged -= ProfileService_ProfileChanged;
-            imageSaveMediator.BeforeImageSaved -= ImageSaveMediator_BeforeImageSaved;
             imageSaveMediator.BeforeFinalizeImageSaved -= ImageSaveMediator_BeforeFinalizeImageSaved;
             return base.Teardown();
         }
@@ -79,62 +65,56 @@ namespace DaleGhent.NINA.MoonAngle {
             RaiseAllPropertiesChanged();
         }
 
-        private async Task ImageSaveMediator_BeforeImageSaved(object sender, BeforeImageSavedEventArgs e) {
-            if (!e.Image.MetaData.Image.ImageType.Contains("DARK") && !e.Image.MetaData.Image.ImageType.Equals("BIAS")) {
-                await e.ImagePrepareTask;
-
-                var observerInfo = new ObserverInfo() {
-                    Latitude = e.Image.MetaData.Observer.Latitude,
-                    Longitude = e.Image.MetaData.Observer.Longitude,
-                    Elevation = e.Image.MetaData.Observer.Elevation,
-                    Temperature = e.Image.MetaData.WeatherData.Temperature,
-                    Humidity = e.Image.MetaData.WeatherData.Humidity,
-                    Pressure = e.Image.MetaData.WeatherData.Pressure,
-                };
-
-                if (!e.Image.MetaData.Image.ImageType.Equals("SNAPSHOT")) {
-                    if (e.Image.MetaData.Target.Coordinates != null && !double.IsNaN(e.Image.MetaData.Target.Coordinates.Dec) && !double.IsNaN(e.Image.MetaData.Target.Coordinates.RADegrees)) {
-                        e.Image.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader(sunAngleKeywordName, Utility.CalculateTargetSeparation(e.Image.MetaData.Target.Coordinates, Enums.SepObject.Sun, observerInfo), string.Format(sunAngleKeywordComment, "object")));
-                        e.Image.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader(moonAngleKeywordName, Utility.CalculateTargetSeparation(e.Image.MetaData.Target.Coordinates, Enums.SepObject.Moon, observerInfo), string.Format(moonAngleKeywordComment, "object")));
-                    }
-                } else {
-                    if (e.Image.MetaData.Telescope.Coordinates != null && !double.IsNaN(e.Image.MetaData.Telescope.Coordinates.Dec) && !double.IsNaN(e.Image.MetaData.Telescope.Coordinates.RADegrees)) {
-                        e.Image.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader(sunAngleKeywordName, Utility.CalculateTargetSeparation(e.Image.MetaData.Telescope.Coordinates, Enums.SepObject.Sun, observerInfo), string.Format(sunAngleKeywordComment, "center")));
-                        e.Image.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader(moonAngleKeywordName, Utility.CalculateTargetSeparation(e.Image.MetaData.Telescope.Coordinates, Enums.SepObject.Moon, observerInfo), string.Format(moonAngleKeywordComment, "center")));
-                    }
-                }
-            }
-        }
-
         private Task ImageSaveMediator_BeforeFinalizeImageSaved(object sender, BeforeFinalizeImageSavedEventArgs e) {
-            if (!e.Image.RawImageData.MetaData.Image.ImageType.Contains("DARK") && !e.Image.RawImageData.MetaData.Image.ImageType.Equals("BIAS")) {
+            var imageType = e.Image.RawImageData.MetaData.Image.ImageType;
 
-                var observerInfo = new ObserverInfo() {
-                    Latitude = e.Image.RawImageData.MetaData.Observer.Latitude,
-                    Longitude = e.Image.RawImageData.MetaData.Observer.Longitude,
-                    Elevation = e.Image.RawImageData.MetaData.Observer.Elevation,
-                    Temperature = e.Image.RawImageData.MetaData.WeatherData.Temperature,
-                    Humidity = e.Image.RawImageData.MetaData.WeatherData.Humidity,
-                    Pressure = e.Image.RawImageData.MetaData.WeatherData.Pressure,
-                };
+            // Don't do anything for darks or biases
+            if (imageType.Contains("DARK") || imageType.Equals("BIAS")) {
+                return Task.CompletedTask;
+            }
 
-                if (!e.Image.RawImageData.MetaData.Image.ImageType.Equals("SNAPSHOT")) {
-                    if (e.Image.RawImageData.MetaData.Target.Coordinates != null && !double.IsNaN(e.Image.RawImageData.MetaData.Target.Coordinates.Dec) && !double.IsNaN(e.Image.RawImageData.MetaData.Target.Coordinates.RADegrees)) {
-                        var sunSep = Utility.CalculateTargetSeparation(e.Image.RawImageData.MetaData.Target.Coordinates, Enums.SepObject.Sun, observerInfo);
-                        var moonSep = Utility.CalculateTargetSeparation(e.Image.RawImageData.MetaData.Target.Coordinates, Enums.SepObject.Moon, observerInfo);
+            // Don't do anything if the observer's location is not set
+            if (double.IsNaN(e.Image.RawImageData.MetaData.Observer.Latitude) ||
+                double.IsNaN(e.Image.RawImageData.MetaData.Observer.Longitude)) {
+                return Task.CompletedTask;
+            }
 
-                        e.AddImagePattern(new ImagePattern(imagePatternSun.Key, imagePatternSun.Description, imagePatternSun.Category) { Value = $"{sunSep:0.00}" });
-                        e.AddImagePattern(new ImagePattern(imagePatternMoon.Key, imagePatternMoon.Description, imagePatternMoon.Category) { Value = $"{moonSep:0.00}" });
-                    }
-                } else {
-                    if (e.Image.RawImageData.MetaData.Telescope.Coordinates != null && !double.IsNaN(e.Image.RawImageData.MetaData.Telescope.Coordinates.Dec) && !double.IsNaN(e.Image.RawImageData.MetaData.Telescope.Coordinates.RADegrees)) {
-                        var sunSep = Utility.CalculateTargetSeparation(e.Image.RawImageData.MetaData.Telescope.Coordinates, Enums.SepObject.Sun, observerInfo);
-                        var moonSep = Utility.CalculateTargetSeparation(e.Image.RawImageData.MetaData.Telescope.Coordinates, Enums.SepObject.Moon, observerInfo);
+            Coordinates targetCoords;
+            var targetName = "object";
 
-                        e.AddImagePattern(new ImagePattern(imagePatternSun.Key, imagePatternSun.Description, imagePatternSun.Category) { Value = $"{sunSep:0.00}" });
-                        e.AddImagePattern(new ImagePattern(imagePatternMoon.Key, imagePatternMoon.Description, imagePatternMoon.Category) { Value = $"{moonSep:0.00}" });
-                    }
+            if (imageType.Equals("SNAPSHOT") && e.Image.RawImageData.MetaData.Telescope.Coordinates != null) {
+                targetCoords = e.Image.RawImageData.MetaData.Telescope.Coordinates;
+            } else if (e.Image.RawImageData.MetaData.Target.Coordinates != null) {
+                targetCoords = e.Image.RawImageData.MetaData.Target.Coordinates;
+
+                if (!string.IsNullOrEmpty(e.Image.RawImageData.MetaData.Target.Name)) {
+                    targetName = e.Image.RawImageData.MetaData.Target.Name;
                 }
+            } else {
+                // Exit if we cannot determine the target's coordinates or where the telescope is pointing
+                return Task.CompletedTask;
+            }
+
+            var observerInfo = new ObserverInfo() {
+                Latitude = e.Image.RawImageData.MetaData.Observer.Latitude,
+                Longitude = e.Image.RawImageData.MetaData.Observer.Longitude,
+                Elevation = e.Image.RawImageData.MetaData.Observer.Elevation,
+                Temperature = e.Image.RawImageData.MetaData.WeatherData.Temperature,
+                Humidity = e.Image.RawImageData.MetaData.WeatherData.Humidity,
+                Pressure = e.Image.RawImageData.MetaData.WeatherData.Pressure,
+            };
+
+            if (!double.IsNaN(targetCoords.Dec) && !double.IsNaN(targetCoords.RADegrees)) {
+                const string keywordComment = "[deg] Angular sep. between {0} and {1}";
+
+                var sunSep = Utility.CalculateTargetSeparation(targetCoords, Enums.SepObject.Sun, observerInfo);
+                var moonSep = Utility.CalculateTargetSeparation(targetCoords, Enums.SepObject.Moon, observerInfo);
+
+                e.Image.RawImageData.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader("SUNANGLE", sunSep, string.Format(keywordComment, targetName, "sun")));
+                e.Image.RawImageData.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader("MOONANGL", moonSep, string.Format(keywordComment, targetName, "moon")));
+
+                e.AddImagePattern(new ImagePattern(imagePatternSun.Key, imagePatternSun.Description, imagePatternSun.Category) { Value = $"{sunSep:0.00}" });
+                e.AddImagePattern(new ImagePattern(imagePatternMoon.Key, imagePatternMoon.Description, imagePatternMoon.Category) { Value = $"{moonSep:0.00}" });
             }
 
             return Task.CompletedTask;
