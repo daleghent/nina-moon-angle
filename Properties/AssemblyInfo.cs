ab0fae4ccb6e1ab4f9ad66c2dddcb3563bf08173
Dale Ghent <daleg@elemental.org>
2024-08-27 21:32:20 -0400

    Update to use NINA 3.1.0.9001

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,10 +20,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Dale Ghent")]
 // The product name that this plugin is part of
 [assembly: AssemblyProduct("Moon Angle")]
-[assembly: AssemblyCopyright("Copyright © 2022 Dale Ghent")]
+[assembly: AssemblyCopyright("Copyright © 2024 Dale Ghent")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.1001")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.1.0.9001")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
